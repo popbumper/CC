79707370d2a663157164f84a8048dc3ec7d05e48
popbumper <thetyke@gmail.com>
2015-10-20 22:14:08 +0100

    444
    44444

--- js/app.js
+++ js/app.js
@@ -1,40 +1,30 @@
 var blogSample = angular.module('blogSample', ['ngRoute','ui.bootstrap']);
 var db;
 var dbCreated = false;
-
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 var app = {
     // Application Constructor
     initialize: function() {
 
-        console.log("initializing");
-        this.bindEvents();
-        console.log("initializing done");
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-
-
-	var fname = document.getElementById("fname").value;
-	var lname = document.getElementById("lname").value;
-	var age = document.getElementById("age").value;
-	var username = document.getElementById("username").value;
-	var password = document.getElementById("psw").value;
-
-	db = window.openDatabase("RegistrationDB", "1.0", "Registration", 200000);
-	if (dbCreated)
-	else
-	db.transaction(populateDB, transaction_error, populateDB_success);
+var viewportScale = 1 / window.devicePixelRatio;
+$("#viewport").attr("content","user-scalable=no, initial-scale="+viewportScale+", minimum-scale=0.2, maximum-scale=2, width=device-width");
 
 
 
