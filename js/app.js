f330d4a209181d60a373e2f9db56fda51c7ad6a5
popbumper <thetyke@gmail.com>
2015-10-20 21:44:11 +0100

    sssaq2
    wqedede

--- js/app.js
+++ js/app.js
@@ -1,23 +1,7 @@
 var blogSample = angular.module('blogSample', ['ngRoute','ui.bootstrap']);
+var db;
+var dbCreated = false;
 
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 var app = {
     // Application Constructor
     initialize: function() {
@@ -39,6 +23,23 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+
+        var viewportScale = 1 / window.devicePixelRatio;
+		$("#viewport").attr("content","user-scalable=no, initial-scale="+viewportScale+", minimum-scale=0.2, maximum-scale=2, width=device-width");
+
+		var fname = document.getElementById("fname").value;
+		var lname = document.getElementById("lname").value;
+		var age = document.getElementById("age").value;
+		var username = document.getElementById("username").value;
+		var password = document.getElementById("psw").value;
+
+		db = window.openDatabase("RegistrationDB", "1.0", "Registration", 200000);
+		if (dbCreated)
+		else
+		db.transaction(populateDB, transaction_error, populateDB_success);
+
+
+
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
@@ -48,5 +49,16 @@ var app = {
 
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
+    },
+
+    onErr: function(error)
+    {
+        alert('Unable to get your location. Without location you will not be able to use navigate feature! Error:' + '\n' + error.message);
+    },
+
+    onSuccess: function(position)
+    {
+        blogSample.latitude = position.coords.latitude;
+        blogSample.longitude = position.coords.longitude;
     }
 };
