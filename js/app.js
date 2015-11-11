746167872e91c8e26fe72efff4969afcc75c34e2
popbumper <thetyke@gmail.com>
2015-11-11 12:14:58 +0000

    mopresasadsa
    asda

--- js/app.js
+++ js/app.js
@@ -1,53 +1,27 @@
 var blogSample = angular.module('blogSample', ['ngRoute','ui.bootstrap']);
 var db;
 var dbCreated = false;
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+
 var app = {
-    // Application Constructor
+
+    findByName: function() {
+        console.log('findByName');
+        this.store.findByName($('.search-key').val(), function(employees) {
+            var l = employees.length;
+            var e;
+            $('.employee-list').empty();
+            for (var i=0; i<l; i++) {
+                e = employees[i];
+                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
+            }
+        });
+    },
+
     initialize: function() {
-
-
-
-
-
-
-
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-    },
-
-    onErr: function(error)
-    {
-        alert('Unable to get your location. Without location you will not be able to use navigate feature! Error:' + '\n' + error.message);
-    },
-
-    onSuccess: function(position)
-    {
-        blogSample.latitude = position.coords.latitude;
-        blogSample.longitude = position.coords.longitude;
+        this.store = new MemoryStore();
+        $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
+
 };
+
+app.initialize();
