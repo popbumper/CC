f0eb9088d3a00b37a12cf76f89710f0130c8c760
popbumper <thetyke@gmail.com>
2016-01-28 21:57:02 +0000

    app.js

--- www/js/app.js
+++ www/js/app.js
@@ -18,13 +18,27 @@ var clubCare = angular.module('clubCare', ['ngRoute','ui.bootstrap']);
  * specific language governing permissions and limitations
  * under the License.
  */
+var Puship=null;
+var watchId=null;
+var currentdeviceid=null;
 var app = {
     // Application Constructor
     initialize: function() {
-		document.addEventListener('deviceready', this.onDeviceReady, false);
-
-        console.log("initializing");
-        console.log("initializing done");
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
@@ -37,15 +51,27 @@ var app = {
 
         console.log('Received Event: ' + id);
 
-		var Puship = window.plugins.puship;
+		Puship = window.plugins.puship;
 		Puship.EnableLog = true;
-		Puship.PushipAppId = Y1vJJkgR9m61W4q; // I.E.: puship_id = "h1mCVGaP9dtGnwG"
+		Puship.EnableHtmlLog = true;
+		Puship.PushipAppId = "Y1vJJkgR9m61W4q"; // I.E.: puship_id = "h1mCVGaP9dtGnwG"
+		Puship.Common.Log("device uuid: " + device.uuid);
+
+		Puship.Common.OnPushReceived(function(event) {
+			console.log("Push received");
+			navigator.notification.alert("Message: " + event.notification.Alert);
+			console.log("Sound: " + event.notification.Sound);
+			console.log("Badge: " + event.notification.Badge);
+			//console.log("Param1: " + event.notification.Param1);
+
+		});
 
 		if (Puship.Common.GetCurrentOs()==Puship.OS.ANDROID){
-			var GCMCode = 102955784975; // This is the senderID provided by Google. I.E.: "28654934133"
+			var GCMCode = "102955784975"; // This is the senderID provided by Google. I.E.: "28654934133"
 			Puship.GCM.Register(GCMCode,
 			{
 				successCallback: function (pushipresult){
+					currentdeviceid=pushipresult.DeviceId;
 					navigator.notification.alert("device registered with DeviceId:" + pushipresult.DeviceId);
 				},
 				failCallback: function (pushipresult){
@@ -56,6 +82,7 @@ var app = {
 			Puship.APNS.Register(
 			{
 				successCallback: function (pushipresult){
+					currentdeviceid=pushipresult.DeviceId;
 					navigator.notification.alert("device registered with DeviceId:" + pushipresult.DeviceId);
 				},
 				failCallback: function (pushipresult){
@@ -66,6 +93,7 @@ var app = {
 			Puship.WP.Register(
 			{
 				successCallback: function (pushipresult){
+					currentdeviceid=pushipresult.DeviceId;
 					navigator.notification.alert("device registered with DeviceId:" + pushipresult.DeviceId);
 				},
 				failCallback: function (pushipresult){
@@ -76,10 +104,195 @@ var app = {
 			Console.log("Not supported platform");
 		}
 
-    }
+    },
+	registerPositionEvent: function()
+	{
+		Puship.Common.RegisterCurrentPosition(
+		{
+			minimumAccuracy: 100,
+			callMinutes: 5,
+			enableHighAccuracy: true,
+			successCallback: function (regresult){
+				console.log("RegisterCurrentPosition done");
+				navigator.notification.alert("RegisterCurrentPosition done");
+			},
+			failCallback: function (regresult){
+				console.log("error during RegisterCurrentPosition: "+ JSON.stringify(regresult));
+				navigator.notification.alert("error during RegisterCurrentPosition: "+ JSON.stringify(regresult));
+			}
+		});
+	},
+	activateWatch: function()
+	{
+		watchId=Puship.Common.WatchPosition({ minimumAccuracy: 5000, callMinutes: 1, enableHighAccuracy: true });
+	},
+	deActivateWatch: function()
+	{
+		if (watchId!=null){
+			Puship.Common.ClearWatch(watchId);
+		}
+	},
+	getPushByCurrentPosition: function(){
+		Puship.Common.GetPushMessages(
+			{
+			//limit: 10, //max limit is 50 default is 20
+			//offset: 100,
+			byCurrentPosition: true,
+			successCallback: function (regresult){
+				console.log("GetPushMessages done");
+
+				if (regresult.length>0)
+				{
+					navigator.notification.alert("Message 1 of "+regresult.length+": "+regresult[0].Message);
+				}else{
+					navigator.notification.alert("No message found");
+				}
+			},
+			failCallback: function (regresult){
+				console.log("error during GetPushMessages by Pos: "+ JSON.stringify(regresult));
+				navigator.notification.alert("error during GetPushMessages by Pos: "+ JSON.stringify(regresult));
+			}
+		});
+	},
+	getPush: function(){
+		Puship.Common.GetPushMessages(
+			{
+			//limit: 10, //max limit is 50 default is 20
+			//offset: 100,
+			successCallback: function (regresult){
+				console.log("GetPushMessages done");
+
+				if (regresult.length>0)
+				{
+					navigator.notification.alert("Message 1 of "+regresult.length+": "+regresult[0].Message);
+				}else{
+					navigator.notification.alert("No message found");
+				}
+			},
+			failCallback: function (regresult){
+				console.log("error during GetPushMessages: "+ JSON.stringify(regresult));
+				navigator.notification.alert("error during GetPushMessages: "+ JSON.stringify(regresult));
+			}
+		});
+	},
+	getPushByLibra: function(){
+		Puship.Common.GetPushMessages(
+			{
+			//limit: 10, //max limit is 50 default is 20
+			//offset: 100,
+			tag: "Libra",
+			successCallback: function (regresult){
+				console.log("GetPushMessages done");
+
+				if (regresult.length>0)
+				{
+					navigator.notification.alert("Message 1 of "+regresult.length+": "+regresult[0].Message);
+				}else{
+					navigator.notification.alert("No message found");
+				}
+			},
+			failCallback: function (regresult){
+				console.log("error during GetPushMessages: "+ JSON.stringify(regresult));
+				navigator.notification.alert("error during GetPushMessages: "+ JSON.stringify(regresult));
+			}
+		});
+	},
+	getPushByDevice: function(){
+		Puship.Common.GetPushMessagesByDevice(
+			{
+			//limit: 10, //max limit is 50 default is 20
+			//offset: 100,
+			successCallback: function (regresult){
+				console.log("GetPushMessagesByDevice done");
+
+				if (regresult.length>0)
+				{
+					navigator.notification.alert("Message 1 of "+regresult.length+": "+regresult[0].Message);
+				}else{
+					navigator.notification.alert("No message found");
+				}
+			},
+			failCallback: function (regresult){
+				console.log("error during GetPushMessagesByDevice: "+ JSON.stringify(regresult));
+				navigator.notification.alert("error during GetPushMessagesByDevice: "+ JSON.stringify(regresult));
+			}
+		});
+	},
+	addLibraFilter: function(){
+		console.log("addLibraFilter");
+		Puship.Common.AddTagFilter("Libra",
+		{
+			successCallback: function (regresult){
+				console.log("Libra filter added");
+				navigator.notification.alert("Libra filter added");
+			},
+			failCallback: function (regresult){
+				console.log("error during addLibraFilter: "+ JSON.stringify(regresult));
+				navigator.notification.alert("error during addLibraFilter: "+ JSON.stringify(regresult));
+			}
+		});
+	},
+	addVirgoFilter: function(){
+		console.log("addVirgoFilter");
+		Puship.Common.AddTagFilter("Virgo",
+		{
+			successCallback: function (regresult){
+				console.log("Virgo filter added");
+				navigator.notification.alert("Virgo filter added");
+			},
+			failCallback: function (regresult){
+				console.log("error during addVirgoFilter: "+ JSON.stringify(regresult));
+				navigator.notification.alert("error during addVirgoFilter: "+ JSON.stringify(regresult));
+			}
+		});
+	},
+	removeFilter: function(){
+		console.log("removeFilter");
+		Puship.Common.RemoveTagFilter("Libra",
+		{
+			successCallback: function (regresult){
+				console.log("Libra filter removed");
+				navigator.notification.alert("Libra filter removed");
+			},
+			failCallback: function (regresult){
+				console.log("error during removeFilter: "+ JSON.stringify(regresult));
+				navigator.notification.alert("error during removeFilter: "+ JSON.stringify(regresult));
+			}
+		});
+	},
+	clearFilters: function(){
+		console.log("removeFilter");
+		Puship.Common.CleanTagFilter(
+		{
+			successCallback: function (regresult){
+				console.log("clearFilters done");
+				navigator.notification.alert("clearFilters done");
+			},
+			failCallback: function (regresult){
+				console.log("error during clearFilters: "+ JSON.stringify(regresult));
+				navigator.notification.alert("error during clearFilters: "+ JSON.stringify(regresult));
+			}
+		});
+	},
+	getFilters: function(){
+		Puship.Common.GetTagFilters(
+			{
+			successCallback: function (regresult){
+				console.log("GetTagFilters done");
+
+				if (regresult.length>0)
+				{
+					navigator.notification.alert("Filter 1 of "+regresult.length+": "+regresult[0]);
+				}else{
+					navigator.notification.alert("No filter found");
+				}
+			},
+			failCallback: function (regresult){
+				console.log("error during GetTagFilters: "+ JSON.stringify(regresult));
+				navigator.notification.alert("error during GetTagFilters: "+ JSON.stringify(regresult));
+			}
+		});
+	}
 };
-
-
-
 
 
